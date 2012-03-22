f77ee97e604186c6b9ea5173bfa7b670f2d609aa
Your Name <you@example.com>
2012-03-22 03:59:44 -0400

    indexed pngs

--- smallautopng.sh
+++ smallautopng.sh
@@ -2,5 +2,7 @@
 for i in $@; do
 	inkscape -b ffffff \
         	 -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;')*390/5376)}") \
-                 -e `basename $i .svg`.png $i
+                 -e `basename $i .svg`.png24 $i
+	convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
+	rm `basename $i .svg`.png24
 done
