66c298c6ab2ee65169672af775e677a15ab04b27
Alexander Rapp <alexander@alexander.co.tz>
2012-04-13 03:32:43 -0400

    animate.sh - add copyright notice

--- animate.sh
+++ animate.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-for i in 1*.png 2*.png; do
-  convert $i $i.gif
+mkdir -p anim
+for i in $1/*.svg; do
+  cp $i anim
 done
-gifsicle -l -d 100 --colors 256 *.png.gif > animated.gif
-rm 1*.png.gif 2*.png.gif
+cd anim
+rm -f 2015.svg 2020.svg
+sed -e's!</svg>!!' -i *.svg
+WIDTH=$(grep width 2010.svg | head -n1 | sed -e's/"$//; s/.*"//;')
+HEIGHT=$(grep height 2010.svg | head -n1 | sed -e's/"$//; s/.*"//;')
+XPOS=$(expr $WIDTH - 2300)
+YPOS=$(expr $HEIGHT - 60)
+for i in *.svg; do
+  cat >> $i <<EOF
+  <text
+     x="$XPOS"
+     y="$YPOS"
+     id="text3057"
+     xml:space="preserve"
+     style="font-size:40px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;font-family:Sans;-inkscape-font-specification:Sans"><tspan
+       x="$XPOS"
+       y="$YPOS"
+       id="tspan3059">©CC-BY-SA Alexander Rapp based on data from OpenStreetMap        http://ugcs.net/~arapp/$1</tspan></text>
+</svg>
+EOF
+  ~/timelines/autopng.sh $i
+  convert `basename $i .svg`.png $i.gif
+done
+gifsicle -l -d 100 --colors 256 *.svg.gif > ../$1/animated.gif
+cd ..
+rm -r anim
