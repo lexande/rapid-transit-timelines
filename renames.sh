7dc005dc4b5d2535636fb8c015b83331d03a32d6
Alexander Rapp <alexander@alexander.co.tz>
2012-04-13 04:17:29 -0400

    remove renames.sh

--- renames.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-START=$(ls *0.svg *5.svg | sort | head -n1 | sed -e's/.*\(....\).svg/\1/')
-LETTER=$(ls *2010.svg | sed -e's/2010.svg//')
-for i in $(seq $START 5 2020); do git mv $LETTER${i}.svg $i.svg; mv $LETTER${i}.png ${i}.png; done
