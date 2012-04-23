450f6dd598354e53642ae54c08811f5e175895d4
Alexander Rapp <alexander@alexander.co.tz>
2012-04-22 22:49:54 -0400

    more checkbox fixes

--- writecheckboxline.sh
+++ writecheckboxline.sh
@@ -3,4 +3,4 @@
 NAME=$(grep Timeline $1/index.html | sed -e's/.*<title>\(.*\) Rapid Transit Timeline.*/\1/')
 UPPER=$(echo $1 | tr 'a-z' 'A-Z')
 
-echo "<input type=\"checkbox\" onclick=\"toggleshow('$UPPER')\" checked=\"unchecked\"><a href=\"$1\">$NAME</a>"
+echo "<input type=\"checkbox\" onclick=\"toggleshow('$UPPER')\" ><a href=\"$1\">$NAME</a>"
