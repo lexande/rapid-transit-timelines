8d9de758cfe4b8214ea0e7e3e464e28e45aa2b9f
Alexander Rapp <alexander@alexander.co.tz>
2012-04-23 03:18:13 -0400

    writecheckboxline

--- writecheckboxline.sh
+++ writecheckboxline.sh
@@ -3,4 +3,4 @@
 NAME=$(grep Timeline $1/index.html | sed -e's/.*<title>\(.*\) Rapid Transit Timeline.*/\1/')
 UPPER=$(echo $1 | tr 'a-z' 'A-Z')
 
-echo "<input type=\"checkbox\" onclick=\"toggleshow('$UPPER')\" ><a href=\"$1\">$NAME</a>"
+echo "<input type=\"checkbox\" id=\"${UPPER}checkbox\" onclick=\"toggleshow('$UPPER')\" ><a href=\"$1\">$NAME</a>"
