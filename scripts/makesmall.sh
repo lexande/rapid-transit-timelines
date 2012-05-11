eedd0c1fedb00b651f78864fe5f8fb7c5413cb93
Alexander Rapp <alexander@alexander.co.tz>
2012-05-11 03:49:17 -0400

    scripts check modification date

--- scripts/makesmall.sh
+++ scripts/makesmall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mkdir -p small
 for i in $@; do
-    sed -e's!tspan2">.*</tspan>!tspan2"></tspan>!;' $i > small/$i
-    cd small
-    ~/timelines/scripts/smallautopng.sh $i
-    cd ..
+    if [ $i -nt small/$i ]; then
+        sed -e's!tspan2">.*</tspan>!tspan2"></tspan>!;' $i > small/$i
+        cd small
+        ~/timelines/scripts/smallautopng.sh $i
+        cd ..
+    fi
 done
--- scripts/smallautopng.sh
+++ scripts/smallautopng.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for i in $@; do
-	inkscape -b ffffff \
-        	 -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;')*390/5376)}") \
+    if [ $i -nt `basename $i .svg`.png ]; then
+        inkscape -b ffffff \
+                 -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;')*390/5376)}") \
                  -e `basename $i .svg`.png24 $i
-	convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
-	rm `basename $i .svg`.png24
+        convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
+        rm `basename $i .svg`.png24
+    fi
 done
