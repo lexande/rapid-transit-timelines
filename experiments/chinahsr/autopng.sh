7d92d0af47dbac60da21005049b1f9c5d7400691
Alexander Rapp <alexander@alexander.co.tz>
2014-09-08 03:47:40 -0400

    chinahsr

---
+++ experiments/chinahsr/autopng.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in $@; do
+    if [ $i -nt `basename $i .svg`.png ]; then
+        inkscape -b ffffff \
+                 -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;'))}") \
+                 -e `basename $i .svg`.png24 $i
+        convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
+        rm `basename $i .svg`.png24
+    fi
+done
