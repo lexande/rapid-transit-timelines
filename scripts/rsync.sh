807751af9af81316736cea32acfc95e1b60ecfc4
Alexander Rapp <alexander@alexander.co.tz>
2014-10-16 01:47:47 -0400

    script updates

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -Pav ~/timelines/timelines --exclude streetmap* --exclude *osm.png arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale --exclude streetmap* --exclude *osm.png arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
