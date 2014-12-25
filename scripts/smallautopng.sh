d154533df3b9031826a4f38a8c79ac65956b31b2
Alexander Rapp <alexander@alexander.co.tz>
2014-12-25 01:43:21 -0500

    pngnq scripts

--- scripts/smallautopng.sh
+++ scripts/smallautopng.sh
@@ -3,8 +3,10 @@ for i in $@; do
     if [ $i -nt `basename $i .svg`.png ]; then
         inkscape -b ffffff \
                  -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;')*390/5376)}") \
-                 -e `basename $i .svg`.png24 $i
-        convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
-        rm `basename $i .svg`.png24
+                 -e `basename $i .svg`.png $i
+#        convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
+#        rm `basename $i .svg`.png24
+        pngnq `basename $i .svg`.png
+        mv `basename $i .svg`-nq8.png `basename $i .svg`.png
     fi
 done
