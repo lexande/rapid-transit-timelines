7dc0b096e51b7d07badf7a776fc0364b29d86054
Alexander Rapp <alexander@alexander.co.tz>
2015-03-09 08:55:35 -0400

    animate.sh URL update

--- scripts/animate.sh
+++ scripts/animate.sh
@@ -4,7 +4,7 @@ for i in $1/*.svg; do
   cp $i anim
 done
 cd anim
-rm -f 2015.svg 2020.svg
+rm -f 2020.svg
 sed -e's!</svg>!!' -i *.svg
 WIDTH=$(grep width 2010.svg | head -n1 | sed -e's/"$//; s/.*"//; s/\..*//;')
 HEIGHT=$(grep height 2010.svg | head -n1 | sed -e's/"$//; s/.*"//; s/\..*//;')
@@ -20,7 +20,7 @@ for i in *.svg; do
      style="font-size:40px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;font-family:Sans;-inkscape-font-specification:Sans"><tspan
        x="$XPOS"
        y="$YPOS"
-       id="tspan3059">©CC-BY-SA Alexander Rapp based on data from OpenStreetMap        http://ugcs.net/~arapp/$1</tspan></text>
+       id="tspan3059">©CC-BY-SA Alexander Rapp based on data from OpenStreetMap        http://alexander.co.tz/$1</tspan></text>
 </svg>
 EOF
   ~/timelines/scripts/autopng.sh $i
