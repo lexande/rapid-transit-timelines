5bf47809497752f3f6660ef686096f23f8cd9d62
Alexander Rapp <alexander@alexander.co.tz>
2016-04-07 01:53:43 -0400

    autopng.sh performance improvement

--- scripts/smallautopng.sh
+++ scripts/smallautopng.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for i in $@; do
-    if [ $i -nt `basename $i .svg`.png ]; then
-        inkscape -b ffffff \
-                 -w $(awk "BEGIN{print int(0.5+$(grep width $i | head -n1 | sed -e's/"$//; s/.*"//;')*390/5376)}") \
-                 -e `basename $i .svg`.png24 $i
-        convert `basename $i .svg`.png24 -type palette PNG8:`basename $i .svg`.png
-        rm `basename $i .svg`.png24
+    b=`basename $i .svg`
+    if [ $i -nt ${b}.png ]; then
+        sed -e's!href=".*osm.png"!href="/dev/null"!' $i > ${b}-osmless.svg
+        inkscape -b ffffff -d 6.52902 -e ${b}.png24 ${b}-osmless.svg
+        convert ${b}.png24 -type palette PNG8:${b}.png
+        rm ${b}.png24 ${b}-osmless.svg
     fi
 done
