567b665fcf8ded78542d36e2a0f4e8804c2fb044
Alexander Rapp <alexander@alexander.co.tz>
2016-04-10 13:24:23 -0400

    Makesmall range support

--- scripts/makesmall.sh
+++ scripts/makesmall.sh
@@ -4,7 +4,18 @@ for i in $@; do
     if [ $i -nt small/$i ]; then
         ~/timelines/scripts/hideyear.pl $i > small/$i
         cd small
-        ~/timelines/scripts/smallautopng.sh $i
+        if grep 'tspan1">....-....' $i; then
+            mv $i temp.svg
+            a=`grep 'tspan1">....-....' temp.svg | sed -e's/.*>\(....\)-.*/\1/'`
+            b=`grep 'tspan1">....-....' temp.svg | sed -e's/.*>....-\(....\).*/\1/'`
+            for k in `seq $a 5 $b`; do
+                 sed -e"s/tspan1\">....-..../tspan1\">$k/" temp.svg > $k.svg
+                 ~/timelines/scripts/smallautopng.sh $k.svg
+            done
+            rm temp.svg
+        else
+            ~/timelines/scripts/smallautopng.sh $i
+        fi
         cd ..
     fi
 done
