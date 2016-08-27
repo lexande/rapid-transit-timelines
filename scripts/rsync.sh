70ca84032435a1839b4561ff4026a6b9fd076653
Alexander Rapp <alexander@alexander.co.tz>
2016-08-27 01:59:07 -0400

    Isochrones

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rsync -Pav ~/timelines/timelines --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -Pav ~/timelines/isochrones --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
 rsync -Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
