0b44c67f54becca804777640847b5ced96fb79e1
Alexander Rapp <alexander@alexander.co.tz>
2016-10-23 17:11:40 -0400

    cleanup

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-~/timelines/scripts/autopng.sh *.svg && 
-~/timelines/scripts/makesmall.sh *.svg &&
-~/timelines/scripts/rsync.sh
--- scripts/writecheckboxline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NAME=$(grep Timeline $1/index.html | sed -e's/.*<title>\(.*\) Rapid Transit Timeline.*/\1/')
-UPPER=$(echo $1 | tr 'a-z' 'A-Z')
-
-echo "<div style=\"display: inline-block\"><input type=\"checkbox\" id=\"${UPPER}checkbox\" onclick=\"toggleshow('$UPPER')\" checked><a href=\"$1\">$NAME</a></div>"
