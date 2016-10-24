6b514bc53eefe56321038ae55b81d8bcc7f8b124
Alexander Rapp <alexander@alexander.co.tz>
2016-10-24 02:15:13 -0400

    scl and hkg year range nonsense

--- scripts/from-year-range.sh
+++ scripts/from-year-range.sh
@@ -9,9 +9,7 @@ if grep 'tspan1">....-....' $infile >/dev/null; then
     b=`grep 'tspan1">....-....' $infile | sed -e's/.*>....-\(....\).*/\1/'`
     echo $infile $dirname $a $b
     for k in `seq $a 5 $b`; do
-	if [ $infile -nt ${dirname}/$k.svg ] || ( ! grep path ${dirname}/$k.svg >/dev/null); then
-            ~/timelines/scripts/hideyear.pl $infile | sed -e"s/tspan1\">....-..../tspan1\">$k/" > ${dirname}/$k.svg
-            gzip -f --keep ${dirname}/$k.svg 
-        fi
+        ~/timelines/scripts/hideyear.pl $infile | sed -e"s/tspan1\">....-..../tspan1\">$k/" > ${dirname}/$k.svg
+        gzip -f --keep ${dirname}/$k.svg 
     done
 fi
