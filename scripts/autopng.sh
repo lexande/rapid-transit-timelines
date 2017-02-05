abf7ddf06f673625e1fb9b438cacf373859a2e37
Alexander Rapp <alexander@alexander.co.tz>
2017-02-05 03:49:13 -0500

    autopng dpi fix

--- scripts/autopng.sh
+++ scripts/autopng.sh
@@ -3,7 +3,7 @@ for i in $@; do
     b=`basename $i .svg`
     if [ $i -nt ${b}.png ]; then
         sed -e's!href=".*osm.png"!href="/dev/null"!' $i > ${b}-osmless.svg
-        inkscape -b ffffff -d 19.5703 -e ${b}.png24 ${b}-osmless.svg
+        inkscape -b ffffff -d 20.875 -e ${b}.png24 ${b}-osmless.svg
         convert ${b}.png24 -type palette PNG8:${b}.png
         rm ${b}.png24 ${b}-osmless.svg
     fi
