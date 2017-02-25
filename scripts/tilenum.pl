666f08cc31a729a6a3ac025eb932f8bb437a35f0
Alexander Rapp <alexander@alexander.co.tz>
2017-02-25 04:00:38 -0500

    tokyo in progress

--- scripts/tilenum.pl
+++ scripts/tilenum.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 
 use Math::Trig;
-$lat = shift;
-$lon = shift;
+@latlong = split("/",shift);
+$lat = $latlong[0];
+$lon = $latlong[1];
 $zoom = 14;
 my $xtile = int( ($lon+180)/360 * 2**$zoom ) ;
 my $ytile = int( (1 - log(tan(deg2rad($lat)) + sec(deg2rad($lat)))/pi)/2 * 2**$zoom ) ;
