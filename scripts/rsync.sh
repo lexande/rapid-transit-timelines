cedc332039e0594833dc8831fdc0b15e21e25c03
Alexander Rapp <alexander@alexander.co.tz>
2017-04-14 00:11:15 -0400

    pyongyang and oneoff index

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -2,4 +2,4 @@
 
 rsync -Pav ~/timelines/timelines --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
 rsync -Pav ~/timelines/isochrones --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
-rsync -Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
+rsync -Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale ~/timelines/experiments/oneoffs --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
