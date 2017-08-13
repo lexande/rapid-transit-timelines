43cd3b8d65f16234a5b58ee92aeb5b3257168f76
Alexander Rapp <alexander@alexander.co.tz>
2017-08-13 01:44:06 -0400

    rsync ipv4

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -Pav ~/timelines/timelines --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
-rsync -Pav ~/timelines/isochrones --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
-rsync -Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale ~/timelines/experiments/oneoffs --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
+rsync -4Pav ~/timelines/timelines --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -4Pav ~/timelines/isochrones --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -4Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale ~/timelines/experiments/oneoffs --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
