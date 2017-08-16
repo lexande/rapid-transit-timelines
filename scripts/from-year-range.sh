fd80301782adde43ee61fc5d9d5cf4e6eb8922de
Alexander Rapp <alexander@alexander.co.tz>
2017-08-16 00:50:19 -0400

    IST year range fix

--- scripts/from-year-range.sh
+++ scripts/from-year-range.sh
@@ -4,12 +4,12 @@ if [ -L $dirname ]; then
     exit
 fi
 infile=`ls $1/*.svg | head -n1`
-if grep 'tspan1">....-....' $infile >/dev/null; then
-    a=`grep 'tspan1">....-....' $infile | sed -e's/.*>\(....\)-.*/\1/'`
-    b=`grep 'tspan1">....-....' $infile | sed -e's/.*>....-\(....\).*/\1/'`
+if grep '>....-....</tspan>' $infile >/dev/null; then
+    a=`grep '>....-....</tspan>' $infile | head -n1 | sed -e's/.*>\(....\)-.*/\1/'`
+    b=`grep '>....-....</tspan>' $infile | head -n1 | sed -e's/.*>....-\(....\).*/\1/'`
     echo $infile $dirname $a $b
     for k in `seq $a 5 $b`; do
-        ~/timelines/scripts/hideyear.pl $infile | sed -e"s/tspan1\">....-..../tspan1\">$k/" > ${dirname}/$k.svg
+        ~/timelines/scripts/hideyear.pl $infile | sed -e"s!>....-....</tspan>!>$k</tspan>!" > ${dirname}/$k.svg
         gzip -f --keep ${dirname}/$k.svg 
     done
 fi
