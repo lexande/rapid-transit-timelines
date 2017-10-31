8a27725cd0b9be292963678232145d452da5352d
Alexander Rapp <alexander@alexander.co.tz>
2017-10-30 23:30:08 -0400

    Widen Asia HSR strokes and move folder

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -4Pav ~/timelines/timelines --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -4Pav ~/timelines/timelines --exclude '*osm*.png' --exclude 'streetmap*' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
 rsync -4Pav ~/timelines/isochrones --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
 rsync -4Pav ~/timelines/experiments/nyc ~/timelines/experiments/streetcarscale ~/timelines/experiments/oneoffs --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
