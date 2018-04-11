a3f6d0aa952580b439ec64e7b3a6d99ab6122fcb
Alexander Rapp <alexander@alexander.co.tz>
2018-04-11 12:27:27 -0400

    walledcityscale makefile

---
+++ experiments/walledcityscale/makeindex.sh
@@ -0,0 +1,73 @@
+cat <<HEREDOC
+<!DOCTYPE HTML>
+<html>
+<head><title>Ancient Walled City Scale Comparison</title>
+<style type="text/css">
+span {
+        margin-top: 5px;
+        margin-bottom: 5px;
+}
+.map {
+        border: 1px solid;
+        margin-left: 5px;
+        margin-right: 5px;
+}
+</style>
+<script language="JavaScript" type="text/javascript">
+function toggleshow(x) {
+	if(document.getElementById(x).style.display=='inline-block') document.getElementById(x).style.display = 'none';
+	else document.getElementById(x).style.display = 'inline-block';
+}
+</script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19998781-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<center>
+<h3>Ancient Walled City Scale Comparison</h3>
+HEREDOC
+for file in $@; do
+  city=`basename $file .svg`
+  NAME=`grep ^$city names | sed -e's/.*\t//'`
+  SNAME=`echo $NAME | sed -e's/<br>.*//; s/,//'`
+  UPPER=$(echo $city | tr 'a-z' 'A-Z')
+  NATIVEW=$(grep '^   width="' $file | head -n1 | sed -e's/.* width="\([0-9\.]*\)".*/\1/;')
+  W=$(awk "BEGIN{print int(0.5+$NATIVEW*0.6)}")
+  H=$(awk "BEGIN{print int(0.5+$(grep ' height=' $file | head -n1 | sed -e's/.* height="\([0-9\.]*\)".*/\1/;')*$W/$NATIVEW)}")
+  echo '<span id="'$UPPER'" style="display: inline-block; vertical-align: middle">'$NAME'<br>'
+  echo '  <img class="map" src="'$file'" title="'$SNAME'" alt="'$SNAME' map" width="'$W'px" height="'$H'px"></span>'
+done
+echo '<p>'
+echo '<form action="">Cities to show:'
+for file in $@; do
+  city=`basename $file .svg`
+  NAME=`grep ^$city names | sed -e's/.*\t//; s/<br>.*//; s/,//;'`
+  UPPER=$(echo $city | tr 'a-z' 'A-Z')
+  echo "<div style=\"display: inline-block\"><input type=\"checkbox\" id=\"${UPPER}checkbox\" onclick=\"toggleshow('$UPPER')\" checked>$NAME</div>"
+done
+cat <<HEREDOC
+</form>
+<p>
+Scale: <svg width="60px" height="3px" style="vertical-align: middle; stroke-width: 0px; background-color: black;"/> = 1 km (60 CSS pixels per km)
+<p>
+Population estimates extremely approximate.  Please send any corrections or questions to threestationsquare at gmail dot com.
+<p>
+See also: <a href="/timelines">rapid transit timelines</a> - <a href="/timelines/misc/">miscellaneous timelines and maps</a>
+<p>
+<div style="font-size: x-small;">By <a href="/">Alexander Rapp</a> based on map data 
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/2.0/"><img alt="CC-BY-SA" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/2.0/80x15.png"></a>
+by <a href="http://www.openstreetmap.org">OpenStreetMap</a> and Wikimedia
+HEREDOC
+echo -n '('
+i=1
+for src in `cat sources`; do
+  echo -n '<a href="'$src'">'$i'</a>,'
+  i=`expr $i + 1`
+done | sed -e's!,$!) contributors.</div></body></html>!'
