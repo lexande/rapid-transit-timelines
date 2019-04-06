4a54c3fb06f6c6782a67b8b8ad881ecd2951c45e
Alexander Rapp <alexander@alexander.co.tz>
2019-04-06 03:20:09 -0400

    alonbracket region 1

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rsync -4Pav ~/timelines/timelines --exclude '*osm*.png' --exclude 'streetmap*' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
-rsync -4Pav ~/timelines/experiments/isochrones ~/timelines/experiments/walledcityscale ~/timelines/experiments/streetcarscale ~/timelines/experiments/oneoffs ~/timelines/experiments/subplans --exclude streetmap* --exclude '*osm*.png' arapp_arapp@ssh.phx.nearlyfreespeech.net:experiments/
+rsync -4Pav ~/timelines/timelines ~/timelines/experiments --exclude streetmap* --exclude '*osm*.png' --exclude azimuth* arapp_arapp@ssh.phx.nearlyfreespeech.net:.
