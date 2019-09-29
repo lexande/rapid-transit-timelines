43d52917f8b00c07a8443b2b785dbf85277446e0
Alexander Rapp <alexander@alexander.co.tz>
2019-09-28 22:33:46 -0400

    rsync top level streetcarscale and walledcityscale

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -4Pav ~/timelines/timelines ~/timelines/experiments --exclude '*streetmap*' --exclude '*osm*.png' --exclude '*azimuth*' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
+rsync -4Pav ~/timelines/timelines ~/timelines/experiments ~/timelines/streetcarscale ~/timelines/walledcityscale --exclude '*streetmap*' --exclude '*osm*.png' --exclude '*azimuth*' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
