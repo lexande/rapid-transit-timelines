781a653cd7263958b09bc7bd49b401cae6a17b64
Alexander Rapp <alexander@alexander.co.tz>
2020-03-28 08:34:53 -0400

    uncrop by default: mel par sao sha

--- timelines/mel/cropscript.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl -wp
-
-s/viewBox="0, 0, 10200, 8600"/viewBox="0, 0, 7050, 8600"/; 
-s/width="10200"/width="7050"/;
--- timelines/sao/cropscript.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -wp
-
-s/viewBox="0, 0, 10800, 7400"/viewBox="0, 2400, 8700, 5000"/; 
-s/height="7400"/height="5000"/; 
-s/width="10800"/width="8700"/;
-s/y="206.960/y="2606.960/;
