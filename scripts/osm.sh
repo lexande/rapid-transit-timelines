16457e2243b5ec14b7e532d47b292552f44f17d1
Alexander Rapp <alexander@alexander.co.tz>
2020-10-07 23:04:15 -0400

    contact info in osm script user agent

--- scripts/osm.sh
+++ scripts/osm.sh
@@ -2,8 +2,8 @@
 for i in `seq $1 $2`; do
 	mkdir $i; cd $i
 	for j in `seq $3 $4`; do
-	echo http://c.tile.openstreetmap.org/14/$i/$j.png
-	done | xargs wget -T 60 -c; cd ..
+	echo https://c.tile.openstreetmap.org/14/$i/$j.png
+	done | xargs wget -U "base maps for https://alexander.co.tz/timelines/ contact threestationsquare@gmail.com" -T 60 -c; cd ..
 done
 for i in `seq $1 $2`; do
 	montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` `seq $3 $4 | sed -e"s!\(.*\)!$i/\1.png!"` $i.png
