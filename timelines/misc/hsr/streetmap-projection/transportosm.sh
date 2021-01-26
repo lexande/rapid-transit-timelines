145ea5686506d82b9390e189b42cff9461a10a28
Alexander Rapp <alexander@alexander.co.tz>
2021-01-26 08:32:02 -0500

    high speed rail 2020

--- timelines/misc/hsr/streetmap-projection/transportosm.sh
+++ timelines/misc/hsr/streetmap-projection/transportosm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 for i in `seq $1 $2`; do
-	mkdir $i; cd $i
+	mkdir -p $i; cd $i
 	for j in `seq $3 $4`; do
-	echo https://a.tile.thunderforest.com/transport/7/$i/$j.png?apikey=13fb05a96fd244349237e1e9093983a7
+		if [ ! -f $j.png ]; then
+			echo https://a.tile.thunderforest.com/transport/7/$i/$j.png?apikey=13fb05a96fd244349237e1e9093983a7
+		fi
 	done | xargs wget -T 60 -c; rename s/.apikey=.*// *.png*; cd ..
 done
 for i in `seq $1 $2`; do
-	montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` $i/*.png $i.png
+	montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` `seq $3 $4 | sed -e's/$/.png/' | sed -e"s!^!$i/!"` $i.png
 done
 montage -mode Concatenate -tile `expr $2 - $1 + 1`x1 `seq $1 $2 | sed -e's/$/.png/'` osm.png
