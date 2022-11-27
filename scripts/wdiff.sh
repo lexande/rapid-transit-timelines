88e6e00d48d2ae443d87053db314262ebf0d15bf
Alexander Rapp <alexander@alexander.co.tz>
2022-11-27 00:07:31 -0500

    wdiff script

---
+++ scripts/wdiff.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wdiff -n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m' <(grep -A1 -B1 "$1" $2 | grep -i '"m' | sort) <(grep -A1 -B1 "$1" $3 | grep -i '"m' | sort)
+wdiff -n -w $'\n'"sed -e's/" -x "" -y "/" -z "/'"$'\n' <(grep -A1 -B1 "$1" $2 | grep -i '"m' | sed -e's/"//g; s! />!!;' | sort) <(grep -A1 -B1 "$1" $3 | grep -i '"m' | sed -e's/"//g; s! />!!;' | sort) | sed -e's! /!/!;' | grep sed
