74e34a62a3e74db3d7c69a5a288cfc580bb8cc8a
Alexander Rapp <alexander@alexander.co.tz>
2023-08-18 04:33:50 +0200

    BER: mostly redraw u-bahn lines

--- scripts/wdiff.sh
+++ scripts/wdiff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wdiff -n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m' <(grep -A1 -B1 "$1" $2 | grep -i '"m' | sort) <(grep -A1 -B1 "$1" $3 | grep -i '"m' | sort)
-wdiff -n -w $'\n'"sed -e's/" -x "" -y "/" -z "/'"$'\n' <(grep -A1 -B1 "$1" $2 | grep -i '"m' | sed -e's! />!!;' | sort) <(grep -A1 -B1 "$1" $3 | grep -i '"m' | sed -e's! />!!;' | sort) | sed -e's! /!/!;' | grep sed
+wdiff -n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m' <(grep -v style $2 | grep -A1 -B1 "$1" | grep -i '"m' | sort) <(grep -v style $3 | grep -A1 -B1 "$1" | grep -i '"m' | sort)
+wdiff -n -w $'\n'"sed -e's/" -x "" -y "/" -z "/'"$'\n' <(grep -v style $2 | grep -A1 -B1 "$1" | grep -i '"m' | sed -e's! />!!;' | sort) <(grep -v style $3 | grep -A1 -B1 "$1" | grep -i '"m' | sed -e's! />!!;' | sort) | sed -e's! /!/!;' | grep sed
