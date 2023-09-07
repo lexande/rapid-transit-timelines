48cbf75f216d1d60761a303e58e541c4ffd02988
Alexander Rapp <alexander@alexander.co.tz>
2023-09-08 00:05:15 +0200

    make preview.gif generation parallelizable

--- scripts/previewgif.sh
+++ scripts/previewgif.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+pushd $1
 mkdir -p anim
 nonempty=false
-for i in $1/*.svg; do
+for i in *.svg; do
   if grep 'stroke-width:[2-5]' $i | grep -v 'stroke:none' >/dev/null; then nonempty=true; fi
   if $nonempty; then ~/timelines/scripts/hideyear.pl $i > anim/`basename $i`; fi
 done
 cd anim
 for i in `ls *.svg | sort -g`; do
-  echo -n "$i "
+  echo -n "$1/$i "
   width=$(grep '^   width=' $i | sed -e's/   width="\(.*\)"/\1/')
   height=$(grep '^   height=' $i | sed -e's/   height="\(.*\)"/\1/')
   sed -e's!href=".*osm.png"!href="/dev/null"!' $i > osmless-${i}
@@ -16,9 +17,10 @@ for i in `ls *.svg | sort -g`; do
 done
 echo ""
 if [ `ls *.svg.gif | wc -l` -gt 1 ]; then
-  gifsicle -l -d 100 --colors 256 `ls *.svg.gif | tail -n1` *.svg.gif > ../$1/preview.gif
+  gifsicle -l -d 100 --colors 256 `ls *.svg.gif | tail -n1` *.svg.gif > ../preview.gif
 else
-  mv *.svg.gif ../$1/preview.gif
+  mv *.svg.gif ../preview.gif
 fi
 cd ..
 rm -r anim
+popd
