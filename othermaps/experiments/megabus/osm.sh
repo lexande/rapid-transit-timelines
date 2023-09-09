6aa0ff0dfaf2993eebd28df99fd2cd851c249b68
Alexander Rapp <alexander@alexander.co.tz>
2023-09-09 22:49:46 +0200

    move various other transit map projects into othermaps

---
+++ othermaps/experiments/megabus/osm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in `seq $1 $2`; do
+	mkdir $i; cd $i
+	for j in `seq $3 $4`; do
+	echo http://c.tile.openstreetmap.org/6/$i/$j.png
+	done | xargs wget -T 60 -c; cd ..
+done
+for i in `seq $1 $2`; do
+	montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` $i/*.png $i.png
+done
+montage -mode Concatenate -tile `expr $2 - $1 + 1`x1 `seq $1 $2 | sed -e's/$/.png/'` osm.png
