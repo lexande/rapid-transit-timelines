11b1d45064397202b99baada8970f925f6a171e9
Alexander Rapp <alexander@alexander.co.tz>
2023-09-10 02:54:43 +0200

    make *osm.sh and tilenum.pl support variable zoom level

--- scripts/railosm.sh
+++ scripts/railosm.sh
@@ -7,10 +7,10 @@ fi
 for i in `seq $1 $2`; do
 	mkdir -p $i; cd $i
 	for j in `seq $3 $4`; do
-	echo https://c.tile.openstreetmap.org/$zoom/$i/$j.png
+	echo https://c.tiles.openrailwaymap.org/standard/$zoom/$i/$j.png
 	done | xargs wget -U "base maps for https://alexander.co.tz/timelines/ contact threestationsquare@gmail.com" -T 60 -c; cd ..
 done
 for i in `seq $1 $2`; do
 	montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` `seq $3 $4 | sed -e"s!\(.*\)!$i/\1.png!"` $i.png
 done
-montage -mode Concatenate -tile `expr $2 - $1 + 1`x1 `seq $1 $2 | sed -e's/$/.png/'` osm.png
+montage -mode Concatenate -tile `expr $2 - $1 + 1`x1 `seq $1 $2 | sed -e's/$/.png/'` railosm.png
--- scripts/rsync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -4Pav ~/timelines/timelines ~/timelines/experiments ~/timelines/streetcarscale ~/timelines/walledcityscale --exclude '*streetmap*' --exclude '*osm*.png' --exclude '*azimuth*' arapp_arapp@ssh.phx.nearlyfreespeech.net:.
