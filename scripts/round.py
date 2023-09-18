629eab0958301da71c1610cc7f370103ec8f903e
Alexander Rapp <alexander@alexander.co.tz>
2023-09-18 12:47:37 +0200

    improvements to wdiff and round scripts

--- scripts/round.py
+++ scripts/round.py
@@ -3,12 +3,17 @@
 import sys
 import re
 
+firstline = True # don't want to round the xml version number!
 
 for line in sys.stdin:
+    if firstline:
+        firstline = False
+        print(line, end='')
+        continue
     nl = ''
     for item in re.split('(\d+\.\d+)', line):
         try:
-            nl += str(round(float(item), 2))
+            nl += re.sub('\.0$','',str(round(float(item), 2)))
         except ValueError:
             nl += item
     print(nl, end='')
--- scripts/wdiff.sh
+++ scripts/wdiff.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-wdiff -n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m' <(grep -v style $2 | grep -A1 -B1 "$1" | grep -i '"m' | sort) <(grep -v style $3 | grep -A1 -B1 "$1" | grep -i '"m' | sort)
-wdiff -n -w $'\n'"sed -e's/" -x "" -y "/" -z "/'"$'\n' <(grep -v style $2 | grep -A1 -B1 "$1" | grep -i '"m' | sed -e's! />!!;' | sort) <(grep -v style $3 | grep -A1 -B1 "$1" | grep -i '"m' | sed -e's! />!!;' | sort) | sed -e's! /!/!;' | grep sed
+
+wdiff -n -w $'\033[30;41m' -x $'\033[0m' -y $'\033[30;42m' -z $'\033[0m' \
+  <(perl -wpe's/\n/ /; s/> />\n/g; s/></>\n</g;' $2 | grep $1 \
+    | perl -wpe's/(\s[a-z]+=)/\n$1/g; s! />!!;' | sort | grep 'd=' )\
+  <(perl -wpe's/\n/ /; s/> />\n/g; s/></>\n</g;' $3 | grep $1 \
+    | perl -wpe's/(\s[a-z]+=)/\n$1/g; s! />!!;' | sort | grep 'd=' )
+wdiff -n -w $'\n'"sed -e's/" -x "" -y "/" -z "/'"$'\n' \
+  <(perl -wpe's/\n/ /; s/> />\n/g; s/></>\n</g;' $2 | grep $1 \
+    | perl -wpe's/(\s[a-z]+=)/\n$1/g; s! />!!;' | sort | grep 'd=' )\
+  <(perl -wpe's/\n/ /; s/> />\n/g; s/></>\n</g;' $3 | grep $1 \
+    | perl -wpe's/(\s[a-z]+=)/\n$1/g; s! />!!;' | sort | grep 'd=' )\
+  | sed -e's! /!/!;' | grep sed
